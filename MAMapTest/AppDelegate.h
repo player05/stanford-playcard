7f48d9fb11188d49737de4063d119e7586716df9
oem <oem@msp.local>
2016-01-08 15:43:29 +0800

    Initial Commit

---
+++ MAMapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MAMapTest
+//
+//  Created by msp on 1/8/16.
+//  Copyright © 2016 zw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MAMapTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MAMapTest
+//
+//  Created by msp on 1/8/16.
+//  Copyright © 2016 zw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
